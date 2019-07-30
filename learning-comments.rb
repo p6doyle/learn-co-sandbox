a68b3fe95d711f5c9a855d63a86bba8bc8ae9692
Peyton Doyle <p6doyle@gmail.com>
2019-07-30 17:12:40 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,12 @@
+# name = "Byron"
+name = "Luca"
+
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in the store."
+
+#=> We're sorry, but per health inspector's rules, Luca is not allowed in the store.
+
+count = 0
+while count < 3 do
+  puts "I am the #{count}, I love to count!"
+  count += 1
+end
